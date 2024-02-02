af5264fd0398e138e0fd51ba29e60c23cbd620ab
Huluette <leana.beuvant@gmail.com>
2024-02-02 12:22:12 +0100

    burger menu work

--- src/BurgerMenu.js
+++ src/BurgerMenu.js
@@ -1,14 +1,24 @@
 import './BurgerMenu.css';
+import React, { useState } from 'react';
 
 export default function BurgerMenu() {
+    // État pour suivre si le menu est ouvert ou fermé
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Fonction pour basculer l'état du menu
+    const toggleMenu = () => setIsOpen(!isOpen);
+
     return (
-            <nav className="burger-menu">
-                <div className="button-close"></div>
+        <>
+            <div className="button-menu" onClick={toggleMenu}></div>
+            <nav className={`burger-menu ${isOpen ? 'visible' : 'hidden'}`}>
+                <div className="button-close" onClick={toggleMenu}></div>
                 <ul className="burger-menu-list">
                     <li className="bar">Présentation</li>
                     <li className="bar">Tarifs</li>
                     <li className="bar">Contact</li>
                 </ul>
             </nav>
+        </>
     );
 }
