28b09aa40fbd10dd30dc2f50f8f72deab4654dac
Huluette <leana.beuvant@gmail.com>
2024-02-05 16:11:06 +0100

    version desktop is ok

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,11 @@
 import './Header.css';
+import React from 'react';
+import Menu from './Menu';
 
 export default function Header() {
     return (
         <header className="header">
+            <Menu/>
             <div className="logo"></div>
             <div className="contact_header">
                 <a className="tel_header" href="#">06 06 XX XX XX</a>
---
+++ src/Menu.js
@@ -0,0 +1,14 @@
+import "./Menu.css";
+
+export default function Menu() {
+
+    return (
+        <div className="menu">
+            <ul className="menu-list">
+                <li className="menu-item">Présentation</li>
+                <li className="menu-item">Tarifs</li>
+                <li className="menu-item">Contact</li>
+            </ul>
+        </div>
+    )
+}
